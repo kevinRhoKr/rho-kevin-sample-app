15480c50b9cbdf77eb1c0b6b99105d6b97fae5ee
kevinRhoKr <kevinrho51@gmail.com>
2022-06-16 08:58:38 -0400

    For PUT operation on Posts

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,8 +1,9 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useRef } from "react";
 import classes from "./Post.module.css";
 
 const Post = (props) => {
   const email = localStorage.getItem("mail");
+  const bodyRef = useRef();
   const [isUpdating, setIsUpdating] = useState(false);
 
   const deleteHandler = () => {
@@ -34,9 +35,37 @@ const Post = (props) => {
   };
 
   const updateHandler = useCallback(() => {
-    setIsUpdating(prev => !prev);
-
+    setIsUpdating((prev) => !prev);
   });
+
+  const updateFetcher = () => {
+    const url = "http://localhost:3000/posts/update";
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        post_id: props.post_id,
+        body: bodyRef.current.value,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        bodyRef.current.value = "";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+      window.location.reload(false);
+  };
 
   return (
     <>
@@ -62,8 +91,16 @@ const Post = (props) => {
                   </small>
                 </span>
                 <small>
-                  {!isUpdating && <a class="text-muted" onClick={updateHandler}>Edit</a>}
-                  {isUpdating && <a class="text-muted" onClick={updateHandler}>Cancel</a>}
+                  {!isUpdating && (
+                    <a class="text-muted" onClick={updateHandler}>
+                      Edit
+                    </a>
+                  )}
+                  {isUpdating && (
+                    <a class="text-muted" onClick={updateHandler}>
+                      Cancel
+                    </a>
+                  )}
                 </small>
               </>
             )}
@@ -72,9 +109,20 @@ const Post = (props) => {
         {!isUpdating && <p class="mb-1">{props.body}</p>}
         {isUpdating && (
           <form>
-          <textarea class="form-control" defaultValue={props.body}></textarea>
-          <br></br>
-          <button type="submit" class="btn btn-primary">Update</button>
+            <textarea
+              class="form-control"
+              defaultValue={props.body}
+              ref={bodyRef}
+            ></textarea>
+            <br></br>
+            <span className={classes.wrapper}>
+              <button type="submit" class="btn btn-primary btn-sm" onClick={updateFetcher}>
+                Update
+              </button>
+              <button class="btn btn-danger btn-sm" onClick={updateHandler}>
+                Cancel
+              </button>
+            </span>
           </form>
         )}
       </a>
